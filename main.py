75fe16384d3cf9a47bc32fcaa1c67bef20e38033
bradybromaghim <142912211+bradybromaghim@users.noreply.github.com>
2024-11-01 13:17:53 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template
+import util
+
+# create an application instance
+# all requests it receives from clients to this object for handling
+# we are instantiating a Flask object by passing __name__ argument to the Flask constructor. 
+# The Flask constructor has one required argument which is the name of the application package. 
+# Most of the time __name__ is the correct value. The name of the application package is used 
+# by Flask to find static assets, templates and so on.
+app = Flask(__name__)
+
+# evil global variables
+# can be placed in a config file
+# here is a possible tutorial how you can do this
+username='bradyb'
+password='test'
+host='127.0.0.1'
+port='5432'
+database='dvdrental'
+
+@app.route('/api/update_basket_a')
+def indexup():
+    # this is your index page
+    # connect to DB
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+    # execute SQL commands
+    util.run_and_commit_sql(cursor, connection, "INSERT INTO basket_a VALUES (5, 'Cherry');")
+    if record == -1:
+        # you can replace this part with a 404 page
+        print('Something is wrong with the SQL command')
+    util.disconnect_from_db(connection,cursor)
+    return render_template('indexup.html')
+    
+@app.route('/')
+# this is how you define a function in Python
+def index():
+    # this is your index page
+    # connect to DB
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+    # execute SQL commands
+    record = util.run_and_fetch_sql(cursor, "SELECT * from customer;")
+    if record == -1:
+        # you can replace this part with a 404 page
+        print('Something is wrong with the SQL command')
+    else:
+        # this will return all column names of the select result table
+        # ['customer_id','store_id','first_name','last_name','email','address_id','activebool','create_date','last_update','active']
+        col_names = [desc[0] for desc in cursor.description]
+        # only use the first five rows
+        log = record[:5]
+        # log=[[1,2],[3,4]]
+    # disconnect from database
+    util.disconnect_from_db(connection,cursor)
+    # using render_template function, Flask will search
+    # the file named index.html under templates folder
+    return render_template('index.html', sql_table = log, table_title=col_names)
+
+@app.route('/api/unique')
+def inde():
+    # this is your index page
+    # connect to DB
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+    # execute SQL commands
+    record = util.run_and_fetch_sql(cursor, "SELECT * from basket_a full join basket_b on basket_a.fruit_a = basket_b.fruit_b where fruit_a is null or fruit_b is null;")
+    if record == -1:
+        # you can replace this part with a 404 page
+        print('Something is wrong with the SQL command')
+    else:
+        # this will return all column names of the select result table
+        # ['customer_id','store_id','first_name','last_name','email','address_id','activebool','create_date','last_update','active']
+        col_names = [desc[0] for desc in cursor.description]
+        # only use the first five rows
+        log = record[:5]
+        # log=[[1,2],[3,4]]
+    # disconnect from database
+    util.disconnect_from_db(connection,cursor)
+    # using render_template function, Flask will search
+    # the file named index.html under templates folder
+    return render_template('index.html', sql_table = log, table_title=col_names)
+
+if __name__ == '__main__':
+	# set debug mode
+    app.debug = True
+    # your local machine ip
+    ip = '127.0.0.1'
+    app.run(host=ip)
+
